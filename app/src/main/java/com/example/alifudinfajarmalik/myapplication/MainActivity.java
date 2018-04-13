edc1f00071faf12b4e3eb5a3aed1bb831cd636b0
alifudinfm <alifudinfajar@gmail.com>
2018-04-13 21:13:47 +0700

    push pertama

---
+++ app/src/main/java/com/example/alifudinfajarmalik/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.alifudinfajarmalik.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText username, password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String u = username.getText().toString();
+                String p = password.getText().toString();
+
+                if(u.equals("Alif") && p.equals("ganteng")){
+                    Toast.makeText(MainActivity.this, "Bener euy", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Salah Euy", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/alifudinfajarmalik/myapplication/orang.java
@@ -0,0 +1,8 @@
+package com.example.alifudinfajarmalik.myapplication;
+
+/**
+ * Created by Alifudin Fajar Malik on 4/13/2018.
+ */
+
+public class orang {
+}
